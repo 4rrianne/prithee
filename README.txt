cb428b90f57bbb9a94b5f5172f762305a25ecb8a
4rrianne <arrianne412@gmail.com>
2026-01-21 21:16:17 -0500

    Configured README.txt

---
+++ README.txt
@@ -0,0 +1,29 @@
+
+CS3140 - Homework 1 - Prithee
+
+Given the sonnet, write code that stops at random words for user to fill in.
+Stop after three correct/incorrect guesses.
+
+Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross’d lovers take their life;
+Whose misadventured piteous overthrows
+Do with their death bury their parents’ strife.
+The fearful passage of their death-mark’d love,
+And the continuance of their parents’ rage,
+Which, but their children’s end, nought could remove,
+Is now the two hours’ traffic of our stage;
+The which if you with patient ears attend,
+What here shall miss, our toil shall strive to mend.
+
+
+Design of the system:
+
+Print random number of words and replace next word with underscores
+    Ask for user's guess
+    Add 1 to correct/incorrect counter if answer does/doesn't match next word
+
+Repeat until either counter reaches 3
---
+++ src/prithee.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class prithee {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+    }
+
+    String sonnet = "Two households, both alike in dignity,\n" +
+            "In fair Verona, where we lay our scene,\n" +
+            "From ancient grudge break to new mutiny,\n" +
+            "Where civil blood makes civil hands unclean.\n" +
+            "From forth the fatal loins of these two foes\n" +
+            "A pair of star-cross’d lovers take their life;\n" +
+            "Whose misadventured piteous overthrows\n" +
+            "Do with their death bury their parents’ strife.\n" +
+            "The fearful passage of their death-mark’d love,\n" +
+            "And the continuance of their parents’ rage,\n" +
+            "Which, but their children’s end, nought could remove,\n" +
+            "Is now the two hours’ traffic of our stage;\n" +
+            "The which if you with patient ears attend,\n" +
+            "What here shall miss, our toil shall strive to mend.\n";
+
+
+}
